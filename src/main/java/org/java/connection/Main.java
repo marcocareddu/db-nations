cbd8eb70a3393747bf1612a4b61ca8f4d39b27d8
marcocareddu <saldumaccu@tuta.io>
2023-11-24 16:26:13 +0100

    Added milestone 3 comments

--- src/main/java/org/java/connection/Main.java
+++ src/main/java/org/java/connection/Main.java
@@ -11,58 +11,62 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
-		
 
-            String url = "jdbc:mysql://localhost:3306/db_nations";
-            String username = "root";
-            String password = "root";
-            String sql = "SELECT countries.name AS Country, country_id AS ID, regions.name AS Region, continents.name As Continent "
-            		+ "FROM countries "
-            		+ "JOIN regions "
-            		+ "ON countries.region_id = regions.region_id "
-            		+ "JOIN continents "
-            		+ "ON regions.continent_id = continents.continent_id ";
-            
-            Connection con = null;
-            
-            Scanner in = new Scanner(System.in);
-            System.out.println("Vuoi cercare per id? ");
-            String wantId = in.nextLine();
-            
-            if(wantId.equals("si"))
-                System.out.println("Inserisci l'id: ");
-                String filterInput = in.nextLine();
-                sql += "WHERE countries.name LIKE " + "'%" + filterInput + "%'";
-            try {
-            	con = DriverManager.getConnection(url, username, password);
-            	
-            	try(PreparedStatement ps = con.prepareStatement(sql)){
-            		try(ResultSet rs = ps.executeQuery()) {
-            			
-            			ArrayList<String[]> tableResults = new ArrayList<>();
-            			tableResults.add(new String[]{"Country", "ID", "Region", "Continent"});
-            			tableResults.add(new String[]{"", "", "", ""});
-            			
-            			while(rs.next()) {
-            				String country = rs.getString(1);
-            				int idInt = rs.getInt(2);
-            				String region = rs.getString(3);
-            				String continent = rs.getString(4);
-            				String id = String.valueOf(idInt);
-            				
-                			tableResults.add(new String[]{country, id, region, continent});
-                	        }
-            	        for (String[] row : tableResults) {
-            	            System.out.printf("%-50s%-10s%-30s%-20s%n", row[0], row[1], row[2], row[3]);
-            			}
-            		}
-            	}
-            } catch (SQLException ex) {
-            	ex.printStackTrace();
-            } finally {
-            	if( con != null) {
-            		con.close();
-            	}
-            }
+//		Database JDBC
+		String url = "jdbc:mysql://localhost:3306/db_nations";
+		String username = "root";
+		String password = "root";
+		String sql = "SELECT countries.name AS Country, country_id AS ID, regions.name AS Region, continents.name As Continent "
+				+ "FROM countries " + "JOIN regions " + "ON countries.region_id = regions.region_id "
+				+ "JOIN continents " + "ON regions.continent_id = continents.continent_id ";
+
+		Connection con = null;
+
+//		User Inputs
+		Scanner in = new Scanner(System.in);
+		System.out.println("Vuoi filtrare il risultato? ");
+		String wantId = in.nextLine();
+
+//		Filtered by characters
+		if (wantId.equals("si"))
+			System.out.println("Inserisci il filtro: ");
+			String filterInput = in.nextLine();
+			
+//		Filtered SQL Query
+		sql += "WHERE countries.name LIKE " + "'%" + filterInput + "%'";
+		
+		try {
+			con = DriverManager.getConnection(url, username, password);
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				try (ResultSet rs = ps.executeQuery()) {
+
+//					List for table printing
+					ArrayList<String[]> tableResults = new ArrayList<>();
+					tableResults.add(new String[] { "Country", "ID", "Region", "Continent" });
+					tableResults.add(new String[] { "", "", "", "" });
+
+					while (rs.next()) {
+						String country = rs.getString(1);
+						int idInt = rs.getInt(2);
+						String region = rs.getString(3);
+						String continent = rs.getString(4);
+						String id = String.valueOf(idInt);
+
+						tableResults.add(new String[] { country, id, region, continent });
+					}
+					
+//					Print results
+					for (String[] row : tableResults) {
+						System.out.printf("%-50s%-10s%-30s%-20s%n", row[0], row[1], row[2], row[3]);
+					}
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
 	}
 }
